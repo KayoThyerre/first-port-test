3a72cf5c3bbadbd70374cf92fa1186fe7fb6f6c0
Kayo Thyerre <kayothyerrework@gmail.com>
2024-06-26 22:26:58 -0300

    header + section cv responsive work on going, plus add bar with "X" using useEffect.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Gif from "./gif";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
-const Header = () => (
-    <header className='header'>
+
+const Header = () => {
+
+  const [isMenuActive, setIsMenuActive] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuActive(prevState => !prevState);
+  };
+
+  return (
+    <header>
       <div className='welcome'>
         <Gif />
         <h1>Kayo <span>Alarcon</span></h1>
       </div>
-  
-      <nav className='nav-container'>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">Sobre</a></li>
-        <li><a href="#resume">Resumo</a></li>
-        <li><a href="#portifolio">Portfolio</a></li>
-        <li><a href="#contact">Contato</a></li>
+
+      <nav>
+        <ul className='nav-container'>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">Sobre</a></li>
+          <li><a href="#resume">Resumo</a></li>
+          <li><a href="#portifolio">Portfolio</a></li>
+          <li><a href="#contact">Contato</a></li>
+        </ul>
+        <i onClick={handleMenuToggle}>
+          <FontAwesomeIcon icon={isMenuActive ? faTimes : faBars} className="fa-solid" />
+        </i>
+
       </nav>
       <div className='btn-div'>
         <a href="#" className='theme-btn'>🌑</a>
       </div>
-      <i className="fa-solid fa-bars"></i>
     </header>
-  );
-  
+  )
+};
+
 export default Header;
