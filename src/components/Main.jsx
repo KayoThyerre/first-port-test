980b81e1afde28d81ed63550c40ec729989ec8b0
Kayo Thyerre <kayothyerrework@gmail.com>
2024-07-14 15:57:22 -0300

    hot-fix

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,26 +4,35 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarDays, faLaptopCode, faLocationDot, faGlobe, faFlag, faPhoneVolume, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Main = () => (
-    <main>
-        <article aria-label='seção sobre mim' className='about'>
-          <img src={myPhoto} alt="imagem de uma pessoa ilutrativa" />
-          <div className="about-content" id="about">
-            <div className="text">
-              <h1>Olá, eu sou o Kayo !</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo pariatur, iusto unde minus quae nam. Placeat excepturi architecto enim tempora?Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo pariatur, iusto unde minus quae nam. Placeat excepturi architecto enim tempora?Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo pariatur, iusto unde minus quae nam. Placeat excepturi architecto enim tempora?</p>
-            </div>
-            <div className="about-info">
-              <p><i><FontAwesomeIcon icon={faCalendarDays} /></i> Aniversário: 06 maio 1996</p>
-              <p><i><FontAwesomeIcon icon={faLaptopCode} /></i> Freelance: Disponível</p>
-              <p><i><FontAwesomeIcon icon={faLocationDot} /></i> Endereço: Guaxupé, MG</p>
-              <p><i><FontAwesomeIcon icon={faGlobe} /></i> Línguas conhecidas: Português, Espanhol, Inglês.</p>
-              <p><i><FontAwesomeIcon icon={faFlag} /></i> Nacionalidade: Brasileiro</p>
-              <p><i><FontAwesomeIcon icon={faPhoneVolume} /></i> Telefone: +55 35 99224-0197</p>
-              <p><i><FontAwesomeIcon icon={faEnvelope} /></i> Email: kayothyerrework@gmail.com</p>
-            </div>
-          </div>
-        </article>
-      </main>
-  );
-  
+  <main>
+    <article aria-label='seção sobre mim' className='about'>
+      <div className="about-title">
+        <h1>Sobre mim</h1>
+        <p>Perfil profissional - Tudo sobre mim</p>
+        <div aria-label='seção ilustrativa de botões'>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <img src={myPhoto} alt="imagem de uma pessoa ilutrativa" />
+      <div className="about-content">
+        <div className="about-text">
+          <h1>Olá, eu sou o Kayo !</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem perspiciatis, voluptatibus atque, nobis dicta sequi doloremque et fugiat ex quasi minus distinctio. Inventore velit officiis accusamus neque asperiores. Ipsa, mollitia.</p>
+        </div>
+        <div className="about-info">
+          <p><i><FontAwesomeIcon icon={faCalendarDays} /></i> Aniversário: 06 maio 1996</p>
+          <p><i><FontAwesomeIcon icon={faLaptopCode} /></i> Freelance: Disponível</p>
+          <p><i><FontAwesomeIcon icon={faLocationDot} /></i> Endereço: Guaxupé, MG</p>
+          <p><i><FontAwesomeIcon icon={faGlobe} /></i> Línguas conhecidas: Português, Espanhol, Inglês.</p>
+          <p><i><FontAwesomeIcon icon={faFlag} /></i> Nacionalidade: Brasileiro</p>
+          <p><i><FontAwesomeIcon icon={faPhoneVolume} /></i> Telefone: +55 35 99224-0197</p>
+          <p><i><FontAwesomeIcon icon={faEnvelope} /></i> Email: kayothyerrework@gmail.com</p>
+        </div>
+      </div>
+    </article>
+  </main>
+);
+
 export default Main;
--- src/components/Proeficiency.jsx
+++ src/components/Proeficiency.jsx
@@ -6,49 +6,40 @@ import { faFileCode } from "@fortawesome/free-solid-svg-icons"; // Usado para o 
 const Proeficiency = () => {
     return (
         <section className="proeficiencies">
-            <div className="about-title">
-            <h1>Sobre mim</h1>
-            <p>Perfil profissional - Tudo sobre mim</p>
-            <div aria-label='seção ilustrativa de botões'>
-              <span></span>
-              <span></span>
-              <span></span>
+            <div className="my-proeficiencies">
+                <ul aria-label="Navegação secundária">
+                    <li>
+                        <i><FontAwesomeIcon icon={faReact} /></i>
+                        <h3>React</h3>
+                        <p>Descrição do React</p>
+                    </li>
+                    <li>
+                        <i><FontAwesomeIcon icon={faHtml5} /></i>
+                        <h3>HTML</h3>
+                        <p>Descrição do HTML</p>
+                    </li>
+                    <li>
+                        <i><FontAwesomeIcon icon={faJsSquare} /></i>
+                        <h3>JavaScript</h3>
+                        <p>Descrição do JavaScript</p>
+                    </li>
+                    <li>
+                        <i><FontAwesomeIcon icon={faFileCode} /></i>
+                        <h3>TypeScript</h3>
+                        <p>Descrição do TypeScript</p>
+                    </li>
+                    <li>
+                        <i><FontAwesomeIcon icon={faCss3Alt} /></i>
+                        <h3>CSS</h3>
+                        <p>Descrição do CSS</p>
+                    </li>
+                    <li>
+                        <i><FontAwesomeIcon icon={faNodeJs} /></i>
+                        <h3>Node.js</h3>
+                        <p>Descrição do Node.js</p>
+                    </li>
+                </ul>
             </div>
-          </div>
-          <div className="my-proeficiencies">
-            <ul aria-label="Navegação secundária">
-                <li>
-                    <i><FontAwesomeIcon icon={faReact} /></i>
-                    <h3>React</h3>
-                    <p>Descrição do React</p>
-                </li>
-                <li>
-                    <i><FontAwesomeIcon icon={faHtml5} /></i>
-                    <h3>HTML</h3>
-                    <p>Descrição do HTML</p>
-                </li>
-                <li>
-                    <i><FontAwesomeIcon icon={faJsSquare} /></i>
-                    <h3>JavaScript</h3>
-                    <p>Descrição do JavaScript</p>
-                </li>
-                <li>
-                    <i><FontAwesomeIcon icon={faFileCode} /></i>
-                    <h3>TypeScript</h3>
-                    <p>Descrição do TypeScript</p>
-                </li>
-                <li>
-                    <i><FontAwesomeIcon icon={faCss3Alt} /></i>
-                    <h3>CSS</h3>
-                    <p>Descrição do CSS</p>
-                </li>
-                <li>
-                    <i><FontAwesomeIcon icon={faNodeJs} /></i>
-                    <h3>Node.js</h3>
-                    <p>Descrição do Node.js</p>
-                </li>
-            </ul>
-          </div>
         </section>
     );
 };
